baf001a7d17bbc373ced229247f8577244fc5350
whodatt1 <codingman875@naver.com>
2024-10-13 14:53:07 +0900

    initial

---
+++ Emergency/src/test/java/com/emergency/web/EmergencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergency.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
